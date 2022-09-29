cf581f6a70dddee923fd628e317e14e1829ef1af
Justin Welsh <justinwelsh33@gmail.com>
2022-09-29 12:31:05 -0500

    added Repository interface that extends JpaRepositories

--- src/main/java/com/justin/traveljournal/repositories/DestinationRepository.java
+++ src/main/java/com/justin/traveljournal/repositories/DestinationRepository.java
@@ -1,2 +1,7 @@
-package com.justin.traveljournal.repositories;public interface DestinationRepository {
+package com.justin.traveljournal.repositories;
+
+import com.justin.traveljournal.models.Destination;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DestinationRepository extends JpaRepository<Destination, Long> {
 }
