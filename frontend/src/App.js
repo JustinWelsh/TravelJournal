923f912d6f4f8a3f7391b06c3318cd759e921e02
Justin Welsh <justinwelsh33@gmail.com>
2022-10-01 01:05:15 -0500

    Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import DestinationCard from "./components/Card/DestinationCard";
 import {useEffect} from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 let destinations = [];
 
@@ -19,19 +20,19 @@ function App() {
             // googleMapsUrl={"https://placeimg.com/400/400/arch"}
    return (
     <div className="App">
-      <header className="App-header">{}
+        <Navbar />
+        <header className="App-header">{}
 
-        <DestinationCard
-            title={"Mount Fuji"}
-            location={"Japan"}
-            googleMapsUrl={""}
-            startDate={"12 Jan, 2021"}
-            endDate={"24 Jan, 2021"}
-            description={"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."}
-            imageUrl={"https://cdn.britannica.com/96/196396-050-13758154/Chureito-Pagoda-Arakura-Sengen-Shrine-Mount-Fuji.jpg"}
-        />
-
-      </header>
+            <DestinationCard
+                title={"Mount Fuji"}
+                location={"Japan"}
+                googleMapsUrl={""}
+                startDate={"12 Jan, 2021"}
+                endDate={"24 Jan, 2021"}
+                description={"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."}
+                imageUrl={"https://cdn.britannica.com/96/196396-050-13758154/Chureito-Pagoda-Arakura-Sengen-Shrine-Mount-Fuji.jpg"}
+            />
+        </header>
     </div>
   );
 }
