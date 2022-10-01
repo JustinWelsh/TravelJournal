c3fe55424e8be861f15e9e3492120d7abc177a71
Justin Welsh <justinwelsh33@gmail.com>
2022-10-01 02:43:35 -0500

    update

--- frontend/src/components/Card/DestinationCard.js
+++ frontend/src/components/Card/DestinationCard.js
@@ -1,11 +1,14 @@
-
+import {MdLocationOn} from "react-icons/md"
 const DestinationCard = (props) => {
     return (
         <>
-            <div className="card lg:card-side bg-base-100 shadow-xl w-3/4 p-8">
+            <div className="card lg:card-side bg-base-100 shadow-xl w-3/4 px-10">
                 <figure><img src={props.imageUrl} alt={`${props.title}-Image`} className={"rounded-2xl shadow-xl"}/></figure>
                 <div className="card-body">
-                    <p>JAPAN</p>
+                <div className={"flex items-center"}>
+                    <MdLocationOn className={"text-[#F55A5A]"}/>
+                    <p className={"grow-0"}>{props.location}</p>
+                </div>
                     <h1 className="card-title text-6xl py-5">{props.title}</h1>
                     <p className={"text-base"}>{props.startDate} - {props.endDate}</p>
                     <p>{props.description}</p>
